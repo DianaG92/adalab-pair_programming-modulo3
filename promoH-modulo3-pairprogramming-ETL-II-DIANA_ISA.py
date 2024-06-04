07eec89cd4783e0016f8fc7d6565628bf278f1ae
isamartineztorrego <martineztorregoisanel@gmail.com>
2024-06-04 15:24:49 +0200

    subida plantilla ETL-II

---
+++ promoH-modulo3-pairprogramming-ETL-II-DIANA_ISA.py
@@ -0,0 +1,18 @@
+# Pair ETL II
+# Ejercicios ETL Parte II
+# NOTA Este ejercicio debe realizarse en un archivo .py
+# Continuarás trabajando con los datos limpios y transformados del pair programming de ayer para crear una base de datos y cargar los datos en ella.
+# Los pasos del ejercicio son:
+# 1. Creación de la Base de Datos:
+    # Utilizar MySQL para crear una base de datos vacía.
+
+
+    # Definir la estructura de la base de datos: esquemas de tablas, relaciones entre ellas, etc.
+
+
+
+# 2. Carga de Datos en la Base de Datos:
+# Utilizar Python + SQL para insertar los datos transformados el día anterior.
+
+
+
